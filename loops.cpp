f224c5d0b9e8298ce6c3c7da0258f85c2ee16dec
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-02 01:11:15 +0530

    roots

--- loops.cpp
+++ loops.cpp
@@ -1,14 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    int a,b,c;
-    cin>>a>>b>>c;
-    if((a>b)&&(a>c))
-    cout<<a<<"is largest";
-    else
-    if((b>a)&&(b>c))
-    cout<<b<<"is largest";
-    else cout<<c<<"is largest";
+int main() {
+
+    float a, b, c, x1, x2, d, real, i;
+    cout << "Enter coefficients a, b and c: ";
+    cin >> a >> b >> c;
+    d = b*b - 4*a*c;
+    
+    if (d > 0) {
+        x1 = (-b + sqrt(d)) / (2*a);
+        x2 = (-b - sqrt(d)) / (2*a);
+        cout << "Roots are real and different." << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+    
+    else if (d == 0) {
+        cout << "Roots are real and same." << endl;
+        x1 = -b/(2*a);
+        cout << "x1 = x2 =" << x1 << endl;
+    }
+
+    else {
+        real = -b/(2*a);
+        i =sqrt(-d)/(2*a);
+        cout << "Roots are complex and different."  << endl;
+        cout << "x1 = " << real << "+" << i << "i" << endl;
+        cout << "x2 = " << real << "-" << i << "i" << endl;
+    }
+
+    return 0;
 }
