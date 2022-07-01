478963393e54848507c4c0c4c98ed83a5c2035a8
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-01 23:07:45 +0530

    vowel/consonant

--- loops.cpp
+++ loops.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    int num;
-    cin>>num;
-    if(num%2==0){
-        cout<<"even";
+    char ch;
+    cin>>ch;
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        cout<<"vowel";
     }
-    else cout<<"odd";
-
+    else cout<<"consonant";
 }
