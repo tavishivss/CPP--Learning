8d90c054ade60bff504be6a1750fe44ec1c5484f
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-04 23:48:56 +0530

    sum

--- loops.cpp
+++ loops.cpp
@@ -1,36 +1,17 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
 
-    float a, b, c, x1, x2, d, real, i;
-    cout << "Enter coefficients a, b and c: ";
-    cin >> a >> b >> c;
-    d = b*b - 4*a*c;
-    
-    if (d > 0) {
-        x1 = (-b + sqrt(d)) / (2*a);
-        x2 = (-b - sqrt(d)) / (2*a);
-        cout << "Roots are real and different." << endl;
-        cout << "x1 = " << x1 << endl;
-        cout << "x2 = " << x2 << endl;
+    int n,sum=0;
+    cout<<"enter number";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        
+        sum=sum+i;
         
     }
-    
-    else if (d == 0) {
-        cout << "Roots are real and same." << endl;
-        x1 = -b/(2*a);
-        cout << "x1 = x2 =" << x1 << endl;
-    }
+            cout<<sum;
 
-    else {
-        real = -b/(2*a);
-        i =sqrt(-d)/(2*a);
-        cout << "Roots are complex and different."  << endl;
-        cout << "x1 = " << real << "+" << i << "i" << endl;
-        cout << "x2 = " << real << "-" << i << "i" << endl;
-    }
-
-    return 0;
 }
