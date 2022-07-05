66ce418e196fd14e14b7af32c15b9072315c11a9
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-05 16:16:08 +0530

    fibonacci

--- loops.cpp
+++ loops.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main()
 {
-  int n,prod=1;
+  //0,1,1,2,3,5;
+  int t1=0,t2=1,n,nt;
   cin>>n;
-
-  for(int i=1;i<=10;i++)
+  cout<<t1<<endl<<t2<<endl;
+  nt=t1+t2;
+  while(nt<=n)
   {
-    prod=n*i;
-    cout<<n<<"*"<<i<<"="<<prod<<endl;
-
+    t1=t2;
+    t2=nt;
+    nt=t1+t2;
+    cout<<nt<<endl;
   }
 }
