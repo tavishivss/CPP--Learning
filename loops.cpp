ee5abe30486fdd669679af5c4935fd10eafab92c
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-05 16:38:43 +0530

    hcf

--- loops.cpp
+++ loops.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main()
 {
-  //0,1,1,2,3,5;
-  int t1=0,t2=1,n,nt;
-  cin>>n;
-  cout<<t1<<endl<<t2<<endl;
-  nt=t1+t2;
-  while(nt<=n)
-  {
-    t1=t2;
-    t2=nt;
-    nt=t1+t2;
-    cout<<nt<<endl;
+  int n1=10,n2=20,i,hcf;
+
+  if(n2>n1){
+    int temp=n1;
+    n1=n2;
+    n2=temp;
   }
+  
+  for(i=1;i<=n2;i++) {
+    if ((n1%i==0)&&(n2%i==0));
+    hcf=i;
+  }
+  cout<<hcf;
 }
