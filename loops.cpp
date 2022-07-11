e5373a4cbff19bcfee3b21271ef9cb46bf9d0d38
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-11 19:46:23 +0530

    factorial

--- loops.cpp
+++ loops.cpp
@@ -1,45 +1,15 @@
 #include<iostream>
-
 using namespace std;
 
- 
+int main()
+{
+  int num,fact=1,i=1;
+  cin>>num;
+  for(i=1;i<=num;i++)
+  {
+    fact=fact*i;
+      
 
-int main() {
-
-// Write your code here
-
-int n;
-
-   cin>>n;
-
-   int oddsum = 0, evensum = 0;
-
-   
-
-   while(n > 0){
-
-       int digit = n % 10;
-
-       if(digit%2){    
-
-          //odd number
-
-           oddsum = oddsum + digit;
-
-       }else{
-
-          //even number
-
-           evensum = evensum + digit;
-
-       }
-
-       n = n / 10;
-
-   }
-
-   cout<<evensum<<" "<<oddsum;
-
+  }
+  cout<<fact;
 }
-
- 
