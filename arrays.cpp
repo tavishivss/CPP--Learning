fda1767924a160ec45c2090a1c8b48d80cdeb56e
Tavishi Vemuri <tavishivss981@gmail.com>
2022-08-04 19:10:25 +0530

    input and output array

--- arrays.cpp
+++ arrays.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 int main()
 {
-  int a[5]={1,2,3,4,5};
-  for(int i=0; i<5; i++)
+    int n;
+    cin>>n;
+  int a[n];
+  for(int i=0; i<n; i++)
   {
-    cout<<a[i]<<" "<<endl;
+    cin>>a[i];
   }
 
+for(int i=0; i<n; i++)
+  {
+    cout<<a[i];
+  }
 }
