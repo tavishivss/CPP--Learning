5da61527ff96317674399edd2444d11b5167d1c3
Tavishi Vemuri <tavishivss981@gmail.com>
2022-08-05 21:56:38 +0530

    heap memory allocation

--- pointers.cpp
+++ pointers.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 int main()
 {
-   int a=10;
    int *p;
-   p=&a;
-   //cout<<a;
-   cout<<p;//address of a
+   p=new int[5];
+   p[0]=1;
+   p[1]=2;
+   p[2]=3;
+   p[3]=4;
+   p[4]=5;
+
+   for(int i=0;i<5;i++)
+   {
+    cout<<p[i]<<" ";
+   }
+   return 0;
 }
