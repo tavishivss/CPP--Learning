063a73e9e61a915d8ffff466f7e4c78460dc082b
Tavishi Vemuri <tavishivss981@gmail.com>
2022-08-05 20:20:39 +0530

    pointers init

--- pointers.cpp
+++ pointers.cpp
@@ -1,16 +1,12 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
 int main()
 {
-    int a=10;
-    int *p; //declared a pointer p;
-    p=&a; //address of a in p;
-
-    cout<<*p<<endl; //value stored in a;
-    int **q=&p; //address of p in pointer of pointer q;
-    cout<<**q<<endl; //value in a;
-    cout<<*q<<endl;  //value in p;
-
-    return 0;
+   int a=10;
+   int *p;
+   p=&a;
+   //cout<<a;
+   cout<<p;//address of a
 }
