795e45a3b3270210ffe5562ae5d878f1e207a132
Tavishi Vemuri <tavishivss981@gmail.com>
2022-09-20 11:51:18 +0530

    recursion intro

--- arrays.cpp
+++ arrays.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main()
 {
-  int arr[5];
-  arr[4]=4;
-  arr[5]=5;
-  cout<<arr[3];
+  int arr[5]={1,2,3,4};
+  cout<<++2[arr];
+  return 0;
 }
---
+++ recursion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void fun1(int n)
+{
+    if(n>0)
+    {
+        cout<<n;
+        fun1(n-1);
+    }
+}
+int main()
+{
+   int x=3;
+   fun1(x);
+}
