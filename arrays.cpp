ff56e24ac558f771e88b3e7ced3fb39465a3a3da
Tavishi Vemuri <tavishivss981@gmail.com>
2022-09-28 18:38:12 +0530

    display elements

--- arrays.cpp
+++ arrays.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 int main()
 {
-  int i,n;
-  cout<<"Size:";
+  int n;
+  cout<<"size";
   cin>>n;
-  int *A;
-  A=new int[n];
-  cout<<"enter elements: ";
-  for(int i=0; i<n;i++)
+  int *A = new int[n];
+    for(int i=0;i<n;i++)
   {
     cin>>A[i];
   }
-  cout<<"elements are:";
-  for(int i=0; i<n; i++)
+
+  cout<<"elements are: ";
+    for(int i=0;i<n;i++)
   {
-    cout<<A[i]<<endl;
+    cout<<A[i];
   }
 }
