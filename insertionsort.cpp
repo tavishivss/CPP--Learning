454734f561f5068c03f2c576928ef17158a17efa
Tavishi Vemuri <tavishivss981@gmail.com>
2022-10-21 19:11:42 +0530

    insertion sort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int main()
+void display(int a[],int n)
 {
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i=0; i<n; i++)
-    {
-        cin>>a[i];
-    }
-
-    for(int i=1; i<n; i++)
-    {
-        int current=a[i];
-        int j=i-1;
-        while(a[j]>current && j>=0)
-        {
-            a[j+1]=a[j];
-            j--;
-        }
-        a[j+1]=current;
-    }
-
-    for(int i=0; i<n; i++)
+    for(int i=0;i<n;i++)
     {
         cout<<a[i]<<" ";
-    }cout<<endl;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+  int a[]={30,20,70,80,10};
+  int n=sizeof(a)/sizeof(a[0]);
+  display(a,n);
+  int i,j,key;
+  for(i=1;i<n;i++)
+  {
+    key=a[i];
+    j=i-1;
+    while(j>=0 && a[j]>key)
+    {
+        a[j+1]=a[j];
+        j=j-1;        
+    }
+    a[j+1]=key;
+  }
+  display(a,n);
+
+
 }
